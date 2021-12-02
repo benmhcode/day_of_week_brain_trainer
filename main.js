7864d697618acd9341e497598df2a1d256bc69a6
benmhcode <85193753+benmhcode@users.noreply.github.com>
2021-12-02 21:08:36 +0000

    outlined

---
+++ main.js
@@ -0,0 +1,39 @@
+// create two arrays - normalYear, one for the leapYear
+
+// onClick event lister for 'generate' button
+// generate date function
+// gregorian calender started on wed sept 2nd and the next day was thur 14th sept 1752
+// generate randomYear between 1752 and 3000
+// check to see if it is a leap year
+// if randomYear%4=0
+// and randomYear%400=0
+// and randomYear%100>0
+// let calender = leapYear
+// else calender = normalYear
+//
+// select correct calender
+// generate random number between 1 and 12 store to varable "randomMonth"
+// access the array and match randomMonth and get the number of days
+// store value as numberOfDays
+// generate randomDay from between 1 and numberOfDays
+
+// order variables randomDay, randomMonth, randomYear to be able to use .day
+
+// check to make sure the day isn't before 14/09/1752
+// if true - repeat function
+
+// .day function to find the day it falls on
+// store as a varable dayOfWeek
+
+// use getElementById and change innerText to display the generated date
+
+// have an event listener to listen for guess
+// store guess as userGuess
+// see if dayOfWeek is === to userGuess
+// If correct, return "Correct!"
+// If incorrect, return `Wrong, is was &{randomDay}`
+
+// have a setting where it will display the doomsdays for randomYear
+// work out a function for generating the doomsdays
+// have a setting where it shows the doomsday pattern and the day that century started on
+//
